778748b581dacf1321e3fc58c6b7970c1b07c440
Laura DeCicco <ldecicco@usgs.gov>
2012-12-19 11:59:30 -0600

    Continue to convert functions to individual files.

---
+++ R/plotConcQ.R
@@ -0,0 +1,55 @@
+#' Plot of Observed Concentration versus Discharge 
+#'
+#' Data come from a data frame named Sample which contains the sample data. 
+#' The metadata come from a data frame named INFO. 
+#' Discharge is plotted on a log scale.
+#'
+#' @param localSample string specifying the name of the data frame that contains the concentration data, default name is Sample
+#' @param localINFO string specifying the name of the data frame that contains the metadata, default name is INFO
+#' @param qUnit object of qUnit class \code{\link{qConst}}, or numeric represented the short code, or character representing the descriptive name.
+#' @param tinyPlot logical variable, if TRUE plot is designed to be plotted small as part of a multipart figure, default is FALSE.
+#' @param concMax number specifying the maximum value to be used on the vertical axis, default is NA (which allows it to be set automatically by the data)
+#' @param printTitle logical variable if TRUE title is printed, if FALSE title is not printed (this is best for a multi-plot figure)
+#' @keywords graphics water-quality statistics
+#' @export
+#' @examples
+#' plotConcQ(localSample = exSampleStart, localINFO = exINFOStart)
+plotConcQ<-function(localSample = Sample, localINFO = INFO, qUnit = 2, tinyPlot = FALSE, concMax = NA, printTitle = TRUE){
+  # this function shows the sample data,
+  # discharge on x-axis on a log scale, concentration on y-axis
+  ################################################################################
+  # I plan to make this a method, so we don't have to repeat it in every funciton:
+  if (is.numeric(qUnit)){
+    qUnit <- qConst[shortCode=qUnit][[1]]
+  } else if (is.character(qUnit)){
+    qUnit <- qConst[qUnit][[1]]
+  }
+  ################################################################################
+  qFactor<-qUnit@qUnitFactor
+  x<-localSample$Q*qFactor
+  yLow<-localSample$ConcLow
+  yHigh<-localSample$ConcHigh
+  Uncen<-localSample$Uncen
+  xMin<-0.95*min(x)
+  xMax<-1.05*max(x)
+  xTicks<-if(tinyPlot) logPretty1(xMin,xMax) else logPretty3(xMin,xMax)
+  numXTicks<-length(xTicks)
+  xLeft<-xTicks[1]
+  xRight<-xTicks[numXTicks]
+  xLab<-qUnit@qUnitExpress
+  maxYHigh<-if(is.na(concMax)) 1.05*max(yHigh) else concMax
+  yTicks<-yPretty(maxYHigh)
+  yTop<-yTicks[length(yTicks)]
+  plotTitle<-if(printTitle) paste(localINFO$shortName,"\n",localINFO$paramShortName,"\n","Concentration versus Discharge") else ""
+  plot(log(x,10),yHigh,axes=FALSE,xlim=c(log(xLeft,10),log(xRight,10)),xaxs="i",xlab=xLab,ylim=c(0,yTop),yaxs="i",ylab="Concentration in mg/L",main=plotTitle,pch=20,cex=0.7,cex.main=1.3,font.main=2,cex.lab=1.2)
+  axis(1,tcl=0.5,at=log(xTicks,10),labels=xTicks)
+  axis(2,tcl=0.5,las=1,at=yTicks)
+  axis(3,tcl=0.5,at=log(xTicks,10),labels=FALSE)
+  axis(4,tcl=0.5,at=yTicks,labels=FALSE)
+  box()
+  yLowVal<-ifelse(is.na(yLow),0,yLow)
+  numSamples<-length(x)
+  uncensoredIndex <- 1:numSamples
+  uncensoredIndex <- uncensoredIndex[Uncen==0]
+  segments(log(x[uncensoredIndex],10),yLowVal[uncensoredIndex],log(x[uncensoredIndex],10),yHigh[uncensoredIndex])
+}
---
+++ R/plotFluxPred.R
@@ -0,0 +1,69 @@
+#' Graph of observed versus estimated flux
+#'
+#' Data come from a data frame named Sample which contains the Sample Data. 
+#' The metadata come from a data frame named INFO.
+#'
+#' @param localSample string specifying the name of the data frame that contains the concentration data, default name is Sample
+#' @param localINFO string specifying the name of the data frame that contains the metadata, default name is INFO
+#' @param fluxUnit number representing entry in pre-defined fluxUnit class array. \code{\link{fluxConst}}
+#' @param fluxMax number specifying the maximum value to be used on the vertical axis, default is NA (which allows it to be set automatically by the data)
+#' @param tinyPlot logical variable, if TRUE plot is designed to be plotted small as part of a multipart figure, default is FALSE.
+#' @param printTitle logical variable if TRUE title is printed, if FALSE not printed (this is best for a multi-plot figure)
+#' @keywords graphics water-quality statistics
+#' @export
+#' @examples
+#' plotFluxPred(localSample = exSampleEnd, localINFO = exINFOEnd, fluxUnit = 7)
+#' plotFluxPred(localSample = exSampleEnd, localINFO = exINFOEnd, fluxUnit = 'poundsDay')
+#' testObject <- new("fluxUnit", shortName="testObject", unitFactor=1.234, unitName="testObjectName", unitExpress=expression("test"),shortCode=100)
+#' plotFluxPred(localSample = exSampleEnd, localINFO = exINFOEnd, fluxUnit = testObject)
+plotFluxPred<-function(localSample = Sample, localINFO = INFO, fluxUnit = 3, fluxMax = NA, tinyPlot = FALSE, printTitle = TRUE){
+  # this function shows observed versus estimated flux
+  # estimated flux on the x-axis (these include the bias correction), 
+  # observed flux on y-axis 
+  # these estimates are from a jack-knife, "leave-one-out", cross validation application of WRTDS
+  ###############
+  # I don't understand the test, RMH 28Jan2011
+  ################
+  
+  ################################################################################
+  # I plan to make this a method, so we don't have to repeat it in every funciton:
+  if (is.numeric(fluxUnit)){
+    fluxUnit <- fluxConst[shortCode=fluxUnit][[1]]    
+  } else if (is.character(fluxUnit)){
+    fluxUnit <- fluxConst[fluxUnit][[1]]
+  }
+  ################################################################################
+  
+  if(tinyPlot) par(mar=c(5,5,1,1)) else par(mar=c(5,5,4,2)+0.1)
+  fluxFactor <- fluxUnit@unitFactor*86.40
+  x<-localSample$ConcHat*localSample$Q*fluxFactor
+  yLow<-localSample$ConcLow*localSample$Q*fluxFactor
+  yHigh<-localSample$ConcHigh*localSample$Q*fluxFactor
+  Uncen<-localSample$Uncen
+  xMax<-1.05*max(x)
+  maxYHigh<-if(is.na(fluxMax)) 1.05*max(yHigh) else fluxMax
+  xTicks<-yPretty(xMax)
+  numXTicks<-length(xTicks)
+  xLeft<-xTicks[1]
+  xRight<-xTicks[numXTicks]
+  yTicks<-yPretty(maxYHigh)
+  numYTicks<-length(yTicks)
+  yBottom<-yTicks[1]
+  yTop<-yTicks[numYTicks]
+  xLab <- fluxUnit@unitEstimate
+  yLab <- fluxUnit@unitExpress
+  plotTitle<-if(printTitle) paste(localINFO$shortName,"\n",localINFO$paramShortName,"\n","Observed vs Estimated Flux") else ""
+  plot(x,yHigh,axes=FALSE,xlim=c(xLeft,xRight),xaxs="i",xlab=xLab,ylim=c(yBottom,yTop),yaxs="i",ylab=yLab,main=plotTitle,pch=20,cex=0.7,cex.main=1.3,font.main=2,cex.lab=1.2)
+  axis(1,tcl=0.5,at=xTicks,labels=xTicks)
+  axis(2,tcl=0.5,las=1,at=yTicks,labels=yTicks)
+  axis(3,tcl=0.5,at=xTicks,labels=FALSE)
+  axis(4,tcl=0.5,at=yTicks,labels=FALSE)
+  box()
+  yLowVal<-ifelse(is.na(yLow),yBottom,yLow)
+  numSamples<-length(x)
+  uncensoredIndex <- 1:numSamples
+  uncensoredIndex <- uncensoredIndex[Uncen==0]
+  segments(x[uncensoredIndex],yLowVal[uncensoredIndex],x[uncensoredIndex],yHigh[uncensoredIndex])
+  abline(a=0,b=1)
+  par(mar=c(5,4,4,2)+0.1)
+}
