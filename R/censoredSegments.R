2aad50cabdb86911711d0479f51b3455fdcbe3c4
Laura DeCicco <ldecicco@usgs.gov>
2013-03-04 14:59:59 -0600

    Created a meaningful example for the censored segment function.

--- R/censoredSegments.R
+++ R/censoredSegments.R
@@ -3,13 +3,31 @@
 #' Basic plotting framework for EGRET dot plots. Graphical parameters default to values that work well with most plots, but all can be re-assigned.
 #' See ?par for complete definitions of most optional input variables.
 #'
-#' @param yBottom vector specifying the x data (required)
-#' @param yLow vector specifying the x data (required)
-#' @param yHigh vector specifying the x data (required)
+#' @param yBottom number specifying minimum flux (required)
+#' @param yLow vector specifying the x data (required), such as ConcLow
+#' @param yHigh vector specifying the x data (required), such as ConcHigh
 #' @param x vector x data (required)
 #' @export
 #' @examples
-#' censoredSegments(c(0,1,2),c(0,1,2),c(0,1,2),c(0,1,2),c(0,1,2))
+#' x <- c(1,2,3,4,5,6)
+#' y <- c(1,3,4,3.3,4.4,2)
+#' xlim <- c(min(x)*.75,max(x)*1.25)
+#' ylim <- c(0,1.25*max(y))
+#' xlab <- "Date"
+#' ylab <- "Concentration"
+#' xTicks <- pretty(xlim)
+#' yTicks <- pretty(ylim)
+#' genericEGRETDotPlot(x=x, y=y, 
+#'                     xlim=xlim, ylim=ylim,
+#'                     xlab=xlab, ylab=ylab,
+#'                     xTicks=xTicks, yTicks=yTicks,
+#'                     plotTitle="Test"
+#' )
+#' yBottom <- 0
+#' yLow <- c(NA,3,4,3.3,4.4,2)
+#' yHigh <- c(1,3,4,3.3,4.4,2)
+#' Uncen <- c(0,1,1,1,1,1)
+#' censoredSegments(yBottom=yBottom,yLow=yLow,yHigh=yHigh,x=x,Uncen=Uncen)
 censoredSegments <- function(yBottom,yLow,yHigh,x,Uncen){
   yLowVal<-ifelse(is.na(yLow),yBottom,yLow)
   numSamples<-length(x)
