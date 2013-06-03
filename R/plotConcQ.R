dbece9df32b88df82032c4d8d59de3002b93f60a
Laura DeCicco <ldecicco@usgs.gov>
2013-06-03 16:30:01 -0500

    Generalized more to allow for plotLogCongQ, giving user more controls.

--- R/plotConcQ.R
+++ R/plotConcQ.R
@@ -8,7 +8,9 @@
 #' @param localINFO string specifying the name of the data frame that contains the metadata, default name is INFO
 #' @param qUnit object of qUnit class \code{\link{qConst}}, or numeric represented the short code, or character representing the descriptive name.
 #' @param tinyPlot logical variable, if TRUE plot is designed to be plotted small as part of a multipart figure, default is FALSE.
+#' @param logScale logical if TRUE x and y plotted in log axis
 #' @param concMax number specifying the maximum value to be used on the vertical axis, default is NA (which allows it to be set automatically by the data)
+#' @param concMin numeric if you want to specify the minimum concentration value to display, you can do so with the argument concMin, otherwise it will be automatic
 #' @param printTitle logical variable if TRUE title is printed, if FALSE title is not printed (this is best for a multi-plot figure)
 #' @param cex number
 #' @param cex.axis number
@@ -20,8 +22,8 @@
 #' Sample <- exSample
 #' INFO <- exINFO
 #' plotConcQ()
-plotConcQ<-function(localSample = Sample, localINFO = INFO, qUnit = 2, tinyPlot = FALSE, 
-                    concMax = NA, printTitle = TRUE, cex=0.8, cex.axis=1.1,cex.main=1.1,...){
+plotConcQ<-function(localSample = Sample, localINFO = INFO, qUnit = 2, tinyPlot = FALSE, logScale=FALSE,
+                    concMax = NA, concMin =NA, printTitle = TRUE, cex=0.8, cex.axis=1.1,cex.main=1.1,...){
   # this function shows the sample data,
   # discharge on x-axis on a log scale, concentration on y-axis
   
@@ -46,26 +48,32 @@ plotConcQ<-function(localSample = Sample, localINFO = INFO, qUnit = 2, tinyPlot 
   if (tinyPlot){
     xLab<-qUnit@qUnitTiny
     yLab <- "Conc. (mg/L)"
-    mar<-c(5,4,1,1.5)
+    par(mar=c(5,4,1,1.5))
   } else {
     xLab<-qUnit@qUnitExpress
     yLab <- "Concentration in mg/L"
-    mar<-c(5,4,4,2)+0.1
+    par(mar=c(5,4,4,2)+0.1)
   }
-
   
+  if(logScale){
+    logScaleText <- "xy"
+    yMin <- concMin
+  } else {
+    logScaleText <- "x"
+    yMin <- 0
+  }
+  
+  yInfo <- generalAxis(x=yHigh, maxVal=concMax, minVal=yMin, tinyPlot=tinyPlot,logScale=logScale)
   xInfo <- generalAxis(x=x, maxVal=NA, minVal=NA, logScale=TRUE, tinyPlot=tinyPlot)
-
-  yInfo <- generalAxis(x=yHigh, maxVal=concMax, minVal=0, tinyPlot=tinyPlot)
   
   genericEGRETDotPlot(x=x, y=yHigh, 
-                      xlim=c(xInfo$bottom, xInfo$top), ylim=c(0,yInfo$top),
+                      xlim=c(xInfo$bottom, xInfo$top), ylim=c(yInfo$bottom,yInfo$top),
                       xlab=xLab, ylab=yLab,
                       xTicks=xInfo$ticks, yTicks=yInfo$ticks,
-                      plotTitle=plotTitle, mar=mar,log="x",cex.axis=cex.axis,cex.main=cex.main, ...
+                      plotTitle=plotTitle, log=logScaleText,cex.axis=cex.axis,cex.main=cex.main, ...
   )
   
-  censoredSegments(0, yLow, yHigh, x, Uncen  )
+  censoredSegments(yInfo$bottom, yLow, yHigh, x, Uncen  )
   
 #   par(originalPar)  
 
