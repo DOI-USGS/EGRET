e6015e1cd95638003e05b31a1eae89bf29bef706
Laura DeCicco <ldecicco@usgs.gov>
2013-06-19 11:26:38 -0500

    Took out postscript command.

--- R/plot15.R
+++ R/plot15.R
@@ -1,10 +1,7 @@
 #' Makes 15 graphs of streamflow statistics on a single page
 #'
 #' Part of flowHistory system.
-#'  This function saves a graphic as a postscript file. 
-#'  It assigns the file a name using the abbreviations for station.
 #'
-#' @param savePath string specifying the full pathname of the folder where the file is to be saved ending with the final slash
 #' @param yearStart A numeric value for year in which the graph should start, default is NA, which indicates that the graph should start with first annual value
 #' @param yearEnd A numeric value for year in which the graph should end, default is NA, which indicates that the graph should end with last annual value
 #' @param localDaily string specifying the name of the data frame that contains the daily data, default is Daily
@@ -14,10 +11,12 @@
 #' @examples
 #' Daily <- ChopDaily
 #' INFO <- ChopINFO
-#' \dontrun{plot15("",yearStart=1990,yearEnd=2000)}
-plot15<-function(savePath,yearStart,yearEnd,localDaily=Daily,localINFO=INFO){
-  plotName<-paste(savePath,"plot15.",localINFO$staAbbrev,".ps",sep="")
-  postscript(file=plotName,width=8,height=10,horizontal=FALSE,family="Helvetica")
+#' pdf("plot15.pdf",heigh=10,width=8)
+#' plot15(yearStart=1990,yearEnd=2000)
+#' dev.off()
+plot15<-function(yearStart,yearEnd,localDaily=Daily,localINFO=INFO){
+#   plotName<-paste(savePath,"plot15.",localINFO$staAbbrev,".ps",sep="")
+#   postscript(file=plotName,width=8,height=10,horizontal=FALSE,family="Helvetica")
   par(mfrow=c(5,3),cex=0.6,oma=c(10,8,10,4),mar=c(1,4,1,1))
   qf<-86/localINFO$drainSqKm
   newINFO<-setPAx(10,12,localINFO=localINFO)
@@ -60,5 +59,5 @@ plot15<-function(savePath,yearStart,yearEnd,localDaily=Daily,localINFO=INFO){
   plot1of15(yearStart,yearEnd,qf,istat=8,localAnnualSeries=newAnnualSeries,isBottom=TRUE)
   caption<-paste("Streamflow statistics (circles) in units of millimeters per day, annual values and seasonal values\nFall (Sept., Oct., and Nov.), Winter (Dec., Jan., and Feb.), Spring (Mar., Apr., and May), and Summer (June, July, and Aug.)\nand locally weighted scatterplot smooth (solid curve) for ",localINFO$shortName," for ",yearStart," - ",yearEnd,".",sep="")
   mtext(caption,side=1,outer=TRUE,line=7,adj=0,font=1,cex=0.7)
-  dev.off()
+#   dev.off()
 }
