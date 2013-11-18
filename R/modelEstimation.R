fb2b228fc46499f26a5754ed336c99ac7410c925
Laura DeCicco <ldecicco@usgs.gov>
2013-11-18 16:28:24 -0600

    Updated the behavior of saving over Daily,Sample, and INFO so that it actually saves over whatever is defined in localDaily, localSample, and localINFO. Also, added arguments to define what surfaces and AnnualResults should be saved as.

--- R/modelEstimation.R
+++ R/modelEstimation.R
@@ -6,15 +6,17 @@
 #' It returns several data frames or matrices (Daily, INFO, Sample, AnnualResults, and surfaces).
 #' AnnualResults is calculated for water year. To use a period of analysis other than water year: AnnualResults<-setupYears(paLong,paStart).
 #'
-#' @param localDaily string specifying the name of the data frame containing the daily values, default is Daily
-#' @param localSample string specifying the name of the data frame containing the sample values, default is Sample
-#' @param localINFO string specifying the name of the data frame containing the metadata, default is INFO
+#' @param localDaily data frame containing the daily values, default is Daily
+#' @param localSample data frame containing the sample values, default is Sample
+#' @param localINFO data frame containing the metadata, default is INFO
 #' @param windowY numeric specifying the half-window width in the time dimension, in units of years, default is 10
 #' @param windowQ numeric specifying the half-window width in the discharge dimension, units are natural log units, default is 2
 #' @param windowS numeric specifying the half-window with in the seasonal dimension, in units of years, default is 0.5
 #' @param minNumObs numeric specifying the miniumum number of observations required to run the weighted regression, default is 100
 #' @param minNumUncen numeric specifying the minimum number of uncensored observations to run the weighted regression, default is 50
-#' @param env environment to set variables in
+#' @param env environment to set returning variables, defaults to parent.frame()
+#' @param localSurface string specifying the name of the returned surface array. If NA (default), array is returned named 'surface'
+#' @param localAnnualResults string specifying the name of the returned AnnualResults calculation. If NA (default), data frame is returned named 'AnnualResults'. setupYears is performed with the water year, and results are saved in this data frame.
 #' @keywords water-quality statistics
 #' @import survival
 #' @export
@@ -23,7 +25,9 @@
 #' Sample <- ChopSample
 #' INFO <- ChopINFO
 #' \dontrun{modelEstimation()}
-modelEstimation<-function(localDaily = Daily,localSample = Sample, localINFO = INFO, windowY=10, windowQ=2, windowS=0.5,minNumObs=100,minNumUncen=50, env=parent.frame()){
+modelEstimation<-function(localDaily = Daily,localSample = Sample, localINFO = INFO, 
+                          windowY=10, windowQ=2, windowS=0.5,minNumObs=100,minNumUncen=50, 
+                          env=parent.frame(),localSurface=NA,localAnnualResults=NA){
   # this code is a wrapper for several different functions that test the model, fit a surface,
   #  estimate daily values and flow normalized daily values
   #  and organize these into monthly results
@@ -32,7 +36,7 @@ modelEstimation<-function(localDaily = Daily,localSample = Sample, localINFO = I
   #
   cat("\n first step running estCrossVal may take about 1 minute")
   Sample1<-estCrossVal(localSample = localSample, windowY, windowQ, windowS, minNumObs, minNumUncen)
-#   cat("\n done with estCrossVal")
+
   surfaceIndexParameters<-surfaceIndex(localDaily = localDaily)
   localINFO$bottomLogQ<-surfaceIndexParameters[1]
   localINFO$stepLogQ<-surfaceIndexParameters[2]
@@ -47,20 +51,43 @@ modelEstimation<-function(localDaily = Daily,localSample = Sample, localINFO = I
   localINFO$minNumUncen<-minNumUncen
   cat("\nNext step running  estSurfaces with survival regression:\n")
   surfaces1<-estSurfaces(localDaily = localDaily, localSample = localSample, windowY, windowQ, windowS, minNumObs, minNumUncen)
-#   cat("\nDone with estSurfaces and starting estDailyFromSurface")
-#   cat("\nThis can take several minutes but you will see updates")
+
   Daily1<-estDailyFromSurfaces(localDaily = localDaily, localINFO = localINFO, localsurfaces = surfaces1)
-  #   cat("\nDone with estDailyFromSurfaces moving on to calculateMonthlyResults")
-  #   MonthlyResults1<-calculateMonthlyResults(localDaily = Daily1)
-  #   cat("\nDone with calculateMonthlyResults")
-  env$Daily<-Daily1
-  env$INFO<-localINFO
-  env$Sample<-Sample1
-  env$surfaces<-surfaces1
-#   localpaLong <- ifelse("paLong" %in% colnames(localINFO),localINFO$paLong, 12)
-#   localpaStart <- ifelse("paStart" %in% colnames(localINFO),localINFO$paStart, 10)
-#   env$AnnualResults<-setupYears(paLong = localpaLong, paStart =localpaStart, localDaily = Daily1)
-  env$AnnualResults<-setupYears(paLong = 12, paStart =10, localDaily = Daily1)
+
+  matchReturn <- match.call(expand.dots = FALSE) # This doesn't return the default arguments
+#   fcall <- mget(names(formals()),sys.frame(sys.nframe()))
+
+  if (is.null(matchReturn$localDaily)){
+    env$Daily<-Daily1
+  } else {
+    assign(as.character(matchReturn$localDaily), Daily1, envir=env)
+  }
+
+  if (is.null(matchReturn$localINFO)){
+    env$INFO<-localINFO
+  } else {
+    assign(as.character(matchReturn$localINFO), localINFO, envir=env)
+  }
+  
+  if (is.null(matchReturn$localSample)){
+    env$Sample<-Sample1
+  } else {
+    assign(as.character(matchReturn$localSample), Sample1, envir=env)
+  }
+  
+  
+  if(is.na(localSurface)){
+    env$surfaces <- surfaces1
+  } else {
+    assign(as.character(matchReturn$localSurface), surfaces1, envir=env)
+  }
+
+  annualResults1 <- setupYears(paLong = 12, paStart = 10, localDaily = Daily1)
+  if(is.na(localAnnualResults)){
+    env$AnnualResults <- annualResults1 
+  } else {
+    assign(as.character(matchReturn$localAnnualResults), annualResults1 , envir=env)
+  }
   
   cat("\nDone with modelEstimation,\nif you want to use a period of analysis other than water year\nnow do AnnualResults<-setupYears(paLong,paStart)\n")
   
