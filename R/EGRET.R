a2f86e90ff55a1c4a69206e72ec6372382819042
Laura DeCicco <ldecicco@usgs.gov>
2014-07-01 13:44:08 -0500

    Updated package date.

--- R/EGRET.R
+++ R/EGRET.R
@@ -4,7 +4,7 @@
 #' Package: \tab EGRET\cr
 #' Type: \tab Package\cr
 #' Version: \tab 1.2.4\cr
-#' Date: \tab 2013-06-28\cr
+#' Date: \tab 2014-06-28\cr
 #' License: \tab Unlimited for this package, dependencies have more restrictive licensing.\cr
 #' Copyright: \tab This software is in the public domain because it contains materials
 #' that originally came from the United States Geological Survey, an agency of
