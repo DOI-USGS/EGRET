7e140a508f427e00e07acc4274c6bab7b486116f
Laura DeCicco <ldecicco@usgs.gov>
2015-08-18 11:29:03 -0500

    Updated example to check if uncensored segments were being drawn correctly. They are.

--- R/censoredSegments.R
+++ R/censoredSegments.R
@@ -14,7 +14,7 @@
 #' @export
 #' @examples
 #' x <- c(1,2,3,4,5,6)
-#' y <- c(1,3,4,3.3,4.4,2)
+#' y <- c(1,3,4,3.3,4.4,7)
 #' xlim <- c(min(x)*.75,max(x)*1.25)
 #' ylim <- c(0,1.25*max(y))
 #' xlab <- "Date"
@@ -28,9 +28,9 @@
 #'                     plotTitle="Test"
 #' )
 #' yBottom <- 0
-#' yLow <- c(NA,3,4,3.3,4.4,2)
-#' yHigh <- c(1,3,4,3.3,4.4,2)
-#' Uncen <- c(0,1,1,1,1,1)
+#' yLow <- c(NA,3,4,3.3,4,7)
+#' yHigh <- c(1,3,4,3.3,5,NA)
+#' Uncen <- c(0,1,1,1,0,0)
 #' censoredSegments(yBottom=yBottom,yLow=yLow,yHigh=yHigh,x=x,Uncen=Uncen)
 censoredSegments <- function(yBottom,yLow,yHigh,x,Uncen,col="black",lwd=1){
   yLowVal<-ifelse(is.na(yLow),yBottom,yLow) #yLow would be NA if "simple" censored....so giving it a value here
