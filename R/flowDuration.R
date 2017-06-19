44cf3dbd903669452fd6e9dfc32ef724e250322a
wdwatkins <wwatkins@usgs.gov>
2017-06-19 16:09:46 -0500

    mostly formatting changes

--- R/flowDuration.R
+++ R/flowDuration.R
@@ -23,56 +23,93 @@
 #' flowDuration(eList,"06-25", qUnit=1,span=30) 
 #' # for a flow-duration curve covering the whole year, expressed in units of csf: 
 #' flowDuration(eList, "01-01", qUnit=2) 
-flowDuration<-function(eList, centerDate = "09-30", qUnit = 2, span = 365) {
-
+flowDuration <- function(eList,
+                         centerDate = "09-30",
+                         qUnit = 2,
+                         span = 365) {
   localINFO <- getInfo(eList)
   localDaily <- getDaily(eList)
   
-  if (is.numeric(qUnit)){
-    qUnit <- qConst[shortCode=qUnit][[1]]
-  } else if (is.character(qUnit)){
+  if (is.numeric(qUnit)) {
+    qUnit <- qConst[shortCode = qUnit][[1]]
+  } else if (is.character(qUnit)) {
     qUnit <- qConst[qUnit][[1]]
   }
   ################################################################################
-  spanFull<-span
-  span<-if(spanFull>182) 182 else spanFull
-  centerDate<-paste("2001-",centerDate)
-  centerDate<-as.Date(centerDate)
-  centerDay<-as.POSIXlt(centerDate)$yday+1
-  startDay<-centerDay-span
-  endDay<-centerDay+span
-  goodDays<-seq(startDay,endDay,1)
-  goodDays<-ifelse(goodDays>0,goodDays,goodDays+365)
-  goodDays<-ifelse(goodDays<366,goodDays,goodDays-365)
-  numDays<-length(localDaily$Day)
-  isGood<-rep(FALSE,numDays)
-  for(i in 1:numDays) {
-    count<-ifelse(localDaily$Day[i]==goodDays,1,0)
-    isGood[i]<-if(sum(count)>0) TRUE else FALSE
+  spanFull <- span
+  span <- if (spanFull > 182) {
+    182
+  } else {
+    spanFull
   }
-  spanDaily<-data.frame(localDaily,isGood)
-  spanDaily<-subset(spanDaily,isGood)
-  n<-length(spanDaily$Day)
-  Q<-spanDaily$Q
-  Q<-sort(Q)
-  index<-c(1,floor(0.05*n),floor(0.1*n),floor(0.25*n),floor(0.5*n),ceiling(0.75*n),ceiling(0.9*n),ceiling(0.95*n),n)
-  QDuration<-c(Q[index[]])
-  QDuration<-QDuration*qUnit@qUnitFactor
-  monthCenter<-as.POSIXlt(centerDate)$mon+1
-  dayCenter<-as.POSIXlt(centerDate)$mday
-  monthStart<-as.POSIXlt(centerDate-span)$mon+1
-  dayStart<-as.POSIXlt(centerDate-span)$mday
-  monthEnd<-as.POSIXlt(centerDate+span)$mon+1
-  dayEnd<-as.POSIXlt(centerDate+span)$mday
-  cat("\nFlow Duration for",localINFO$shortName,"\n")
-  # 	if(spanFull>182) cat("\nFlow duration is based on full year") else cat("\nFlow duration period is centered on",monthFull[monthCenter],dayCenter,"\nAnd spans the period from",monthFull[monthStart],dayStart," To",monthFull[monthEnd],dayEnd)
-  if(spanFull>182) cat("\nFlow duration is based on full year") else cat("\nFlow duration period is centered on",monthInfo[[monthCenter]]@monthFull,dayCenter,"\nAnd spans the period from",monthInfo[[monthStart]]@monthFull,dayStart," To",monthInfo[[monthEnd]]@monthFull,dayEnd)
-  cat("\n\nDischarge units are",qUnit@qUnitName,"\n")
-  Qprint<-format(QDuration,digits=3,width=9)
-#   cat("\n\n       min        5%       10%       25%       50%       75%       90%       95%       max")
-#   cat("\n",Qprint,"\n")	
   
-  names(QDuration) <- c("min","5%","10%","25%","50%","75%","90%","95%","max")
+  centerDate <- paste("2001-", centerDate)
+  centerDate <- as.Date(centerDate)
+  centerDay <- as.POSIXlt(centerDate)$yday + 1
+  
+  startDay <- centerDay - span
+  endDay <- centerDay + span
+  
+  goodDays <- seq(startDay, endDay, 1)
+  goodDays <- ifelse(goodDays > 0, goodDays, goodDays + 365)
+  goodDays <- ifelse(goodDays < 366, goodDays, goodDays - 365)
+  
+  numDays <- length(localDaily$Day)
+  isGood <- rep(FALSE, numDays)
+  for (i in 1:numDays) {
+    count <- ifelse(localDaily$Day[i] == goodDays, 1, 0)
+    isGood[i] <- if (sum(count) > 0) TRUE else FALSE
+  }
+  spanDaily <- data.frame(localDaily, isGood)
+  spanDaily <- subset(spanDaily, isGood)
+  
+  n <- length(spanDaily$Day)
+  Q <- spanDaily$Q
+  Q <- sort(Q)
+  index <-c(
+    1,
+    floor(0.05 * n),
+    floor(0.1 * n),
+    floor(0.25 * n),
+    floor(0.5 * n),
+    ceiling(0.75 * n),
+    ceiling(0.9 * n),
+    ceiling(0.95 * n),
+    n
+  )
+  QDuration <- c(Q[index[]])
+  QDuration <- QDuration * qUnit@qUnitFactor
+  
+  monthCenter <- as.POSIXlt(centerDate)$mon + 1
+  dayCenter <- as.POSIXlt(centerDate)$mday
+  monthStart <- as.POSIXlt(centerDate - span)$mon + 1
+  dayStart <- as.POSIXlt(centerDate - span)$mday
+  monthEnd <- as.POSIXlt(centerDate + span)$mon + 1
+  dayEnd <- as.POSIXlt(centerDate + span)$mday
+  cat("\nFlow Duration for", localINFO$shortName, "\n")
+  # 	if(spanFull>182) cat("\nFlow duration is based on full year") else cat("\nFlow duration period is centered on",monthFull[monthCenter],dayCenter,"\nAnd spans the period from",monthFull[monthStart],dayStart," To",monthFull[monthEnd],dayEnd)
+  if (spanFull > 182){
+    cat("\nFlow duration is based on full year")
+  } else {
+    cat(
+      "\nFlow duration period is centered on",
+      monthInfo[[monthCenter]]@monthFull,
+      dayCenter,
+      "\nAnd spans the period from",
+      monthInfo[[monthStart]]@monthFull,
+      dayStart,
+      " To",
+      monthInfo[[monthEnd]]@monthFull,
+      dayEnd
+    )
+  }
+  cat("\n\nDischarge units are", qUnit@qUnitName, "\n")
+  Qprint <- format(QDuration, digits = 3, width = 9)
+  #   cat("\n\n       min        5%       10%       25%       50%       75%       90%       95%       max")
+  #   cat("\n",Qprint,"\n")
+  
+  names(QDuration) <-
+    c("min", "5%", "10%", "25%", "50%", "75%", "90%", "95%", "max")
   
   return(QDuration)
 }
--- R/makeRandomResiduals.R
+++ R/makeRandomResiduals.R
@@ -7,20 +7,22 @@
 #' @param eList named list with at least the Sample dataframe
 #' @keywords water-quality statistics
 #' @importFrom truncnorm rtruncnorm
+#' @examples 
+#' choptankAugmented <- makeAugmentedSample(Choptank_eList)
 #' @export
 #' @return eList named list with modified Sample data frame.
 makeAugmentedSample <- function(eList){
-
+  
   if(all(c("SE","yHat") %in% names(eList$Sample))){
     localSample <- eList$Sample
-    numSamples<-length(localSample$Uncen)
+    numSamples <- length(localSample$Uncen)
     a <- ifelse(localSample$Uncen==0&!is.na(localSample$ConcLow),log(localSample$ConcLow)-localSample$yHat,-Inf)
     b <- ifelse(localSample$Uncen==1,+Inf,log(localSample$ConcHigh) - localSample$yHat)
     mean <- ifelse(localSample$Uncen==1,log(localSample$ConcHigh) - localSample$yHat,0)
     sd <- ifelse(localSample$Uncen==1,0,localSample$SE)
-    localSample$rResid<-truncnorm::rtruncnorm(numSamples,a,b,mean,sd)
+    localSample$rResid <- rtruncnorm(numSamples,a,b,mean,sd)
     localSample$rObserved <- exp(localSample$rResid + localSample$yHat)
-
+    
     eList	<- as.egret(eList$INFO, eList$Daily, localSample, eList$surfaces)
   } else {
     message("Pseudo only supported after running modelEstimation")
