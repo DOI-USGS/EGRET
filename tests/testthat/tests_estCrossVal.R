4ef388edbf608bf998f7cbe910b0b39b5731c978
Lindsay Carr <lcarr@usgs.gov>
2017-06-21 15:10:24 -0500

    tests for estCrossVal (called from modelEstimation)

---
+++ tests/testthat/tests_estCrossVal.R
@@ -0,0 +1,39 @@
+context("testing estCrossVal")
+
+test_that("estCrossVal adds correct, new columns", {
+  
+  # Choptank_eList has model results already, but the model 
+  # hasn't been run since it was originally saved.
+  eList <- Choptank_eList
+  info_stale <- getInfo(eList)
+  daily_stale <- getDaily(eList)
+  sample_stale <- getSample(eList)
+  
+  info_orig <- info_stale[, 1:(which(names(info_stale) == "bottomLogQ") - 1)]
+  daily_orig <- daily_stale[, 1:(which(names(daily_stale) == "yHat") - 1)]
+  sample_orig <- sample_stale[, 1:(which(names(sample_stale) == "yHat") - 1)]
+  surfaces_orig <- NA
+  eList_orig <- mergeReport(info_orig, daily_orig, sample_orig, surfaces_orig)
+  
+  # execute cross validation
+  numDays <- length(daily_orig$DecYear)
+  DecLow <- daily_orig$DecYear[1]
+  DecHigh <- daily_orig$DecYear[numDays]
+  sample_crossval <- estCrossVal(numDays,DecLow,DecHigh,sample_orig)
+  
+  # estCrossVal adds three columns to Sample
+  expect_equal(ncol(sample_crossval) - ncol(sample_orig), 3)
+  expect_false(any(c("yHat", "SE", "ConcHat") %in% names(sample_orig)))
+  expect_true(all(c("yHat", "SE", "ConcHat") %in% names(sample_crossval)))
+  
+  # test that no original columns were lost in estCrossVal
+  expect_true(all(names(sample_orig) %in% names(sample_crossval)))
+  
+  # verify that values of yHat, SE, and ConcHat are what they should be
+  expect_equal(mean(sample_crossval$yHat), 0.0475113943)
+  expect_equal(mean(sample_crossval$SE), 0.2629914096)
+  expect_equal(mean(sample_crossval$ConcHat), 1.1272620557)
+  
+})
+
+
