582dd771e841624198ef378395685e9a34d831fe
Laura DeCicco <ldecicco@usgs.gov>
2017-08-03 13:28:48 -0500

    More tests

--- tests/testthat/tests_flow_normalization.R
+++ tests/testthat/tests_flow_normalization.R
@@ -52,3 +52,37 @@ test_that("setupYears", {
   expect_equal(signif(ar_calendarYear_fn$FNConc[1], digits = 7), 1.021644)
   expect_equal(signif(ar_calendarYear_fn$FNFlux[1], digits = 7), 271.6512)
 })
+
+test_that("setupYears", {
+  testthat::skip_on_cran()
+  
+  eList <- Choptank_eList
+  
+  tC <- tableChange(eList)
+  expect_null(tC)
+  
+  tCS <- tableChangeSingle(eList)
+  expect_equal(ncol(tCS), 6)
+  expect_equal(tCS$Year1, c(1981,1981,1981,1981,1981,1981,1986,1986,1986,1986,1986,1991,
+                            1991,1991,1991,1996,1996,1996,2001,2001,2006))
+  
+  expect_equal(tCS$Year2, c(1986,1991,1996,2001,2006,2011,1991,1996,2001,2006,2011,1996,
+                            2001,2006,2011,2001,2006,2011,2006,2011,2011))
+  
+  expect_equal(tCS$`change[mg/L]`[1], 0.039)
+  expect_equal(tCS$`slope[mg/L/yr]`[1], 0.0079)
+  expect_equal(tCS$`change[%]`[1], 3.9)
+  expect_equal(tCS$`slope [%/yr]`[1], 0.79)
+  
+  tR <- tableResults(eList)
+  expect_true(all(names(tR) %in% c("Year","Discharge [cms]",    
+                                   "Conc [mg/L]","FN Conc [mg/L]",    
+                                   "Flux [10^6kg/yr]","FN Flux [10^6kg/yr]")))
+  
+  expect_equal(tR$Year[1], 1980)
+  expect_equal(tR$`Discharge [cms]`[1], 4.25)
+  expect_equal(tR$`Conc [mg/L]`[1], 0.949)
+  expect_equal(tR$`FN Conc [mg/L]`[1], 1.003)
+  expect_equal(tR$`Flux [10^6kg/yr]`[1], 0.1154)
+  expect_equal(tR$`FN Flux [10^6kg/yr]`[1], 0.106)
+})
