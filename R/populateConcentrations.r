dcb2020d566771ba5de290c878d11193104b2bdb
Oliver <soliver@usgs.gov>
2017-10-04 08:25:29 -0500

    Recognize the greater than symbol as a censored value, set ConcHigh to zero in these instances

--- R/populateConcentrations.r
+++ R/populateConcentrations.r
@@ -14,8 +14,9 @@ populateConcentrations <- function(rawData){  # rawData is a dataframe with valu
   concentrationColumns <- as.data.frame(matrix(ncol=3,nrow=length(rawData$value)))
   colnames(concentrationColumns) <- c('ConcLow','ConcHigh','Uncen')  
   concentrationColumns$ConcLow <- as.numeric(ifelse((rawData$code!="<" | is.na(rawData$code)),rawData$value,0))
-  concentrationColumns$ConcHigh <- as.numeric(rawData$value)
+  concentrationColumns$ConcHigh <- as.numeric(ifelse((rawData$code != ">" | is.na(rawData$code)), rawData$value, 0))
   tempConcLow<-ifelse((rawData$code!="<" | is.na(rawData$code)),rawData$value,0)
+  tempConcHigh <- ifelse((rawData$code != ">" | is.na(rawData$code)), rawData$value, 0)
   concentrationColumns$Uncen <- ifelse(tempConcLow==0,0,1)  
   #Add if value = NA?
   return (concentrationColumns)  # returns ConcLow, ConcHigh, Uncen (0 if censored, 1 if uncensored)
