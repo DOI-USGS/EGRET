5ee4bd208f93126eb23bf4d7ff67c1e7ae955d67
Laura DeCicco <ldecicco@usgs.gov>
2018-04-04 13:54:34 -0500

    Getting rid of phos data

--- R/stitch.R
+++ R/stitch.R
@@ -28,16 +28,16 @@
 #' The modified method tends to reduce curvature near the start and end of record.  Default is TRUE.
 #' @export
 #' @examples 
-#' eList <- Choptank_Phos
+#' eList <- Choptank_eList
 #' 
 #' surfaceStart <- "1986-10-01"
-#' surfaceEnd <- "2012-09-30"
+#' surfaceEnd <- "2010-09-30"
 #' 
 #' # Surface skips a few years:
 #' sample1StartDate <- "1986-10-01"
 #' sample1EndDate <- "1992-09-30"
 #' sample2StartDate <- "1996-10-01"
-#' sample2EndDate <- "2012-09-30"
+#' sample2EndDate <- "2011-09-30"
 #' 
 #' \dontrun{
 #' surface_skip <- stitch(eList, 
@@ -49,7 +49,7 @@
 #' sample1StartDate <- "1986-10-01"
 #' sample1EndDate <- "1996-09-30"
 #' sample2StartDate <- "1992-10-01"
-#' sample2EndDate <- "2012-09-30"
+#' sample2EndDate <- "2011-09-30"
 #' 
 #' surface_overlap <- stitch(eList, 
 #'                          sample1StartDate, sample1EndDate, 
