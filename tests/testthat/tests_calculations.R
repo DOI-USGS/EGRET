3d83139934f731eeaa85891ab0bd2e7422dfb343
Laura <ldecicco@usgs.gov>
2020-10-15 13:05:50 -0500

    Fix tests

--- tests/testthat/tests_calculations.R
+++ tests/testthat/tests_calculations.R
@@ -5,9 +5,9 @@ test_that("calculateMonthly returns expected type/format", {
   expect_is(results, "data.frame")
   expect_gt(nrow(results), 1)
   expect_is(results$Q, "numeric")
-  expect_equal(names(results), c("Month", "Year", "DecYear", "Q",          
-                                 "Conc", "Flux", "FluxTotal", "FNConc",     
-                                 "FNFlux", "FNFluxTotal"))
+  expect_true(all(names(results) %in% c("Month", "Year", "DecYear", "Q",          
+                                 "Conc", "Flux", "FNConc",     
+                                 "FNFlux")))
 })
 
 test_that("flowDuration returns expected type, and arguments change output", {
