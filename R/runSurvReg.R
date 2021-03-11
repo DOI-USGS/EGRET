691afd1088c6010e42702104528df561f339f5e6
DeCicco <ldecicco@usgs.gov>
2021-03-11 13:21:40 -0600

    Make sure survModel gets returned

--- R/runSurvReg.R
+++ R/runSurvReg.R
@@ -176,11 +176,13 @@ run_WRTDS <- function(estY, estLQ,
   }, warning=function(w) {
     
     if(w$message == "Ran out of iterations and did not converge"){
-      
       Sam2 <- jitterSam(Sam)
-      survModel <- survival::survreg(survival::Surv(log(ConcLow),log(ConcHigh),type="interval2") ~ 
-                                       DecYear+LogQ+SinDY+CosDY,data=Sam2,weights=weight,dist="gaus")
-    } 
+    } else {
+      Sam2 <- Sam
+    }
+    
+    survModel <- survival::survreg(survival::Surv(log(ConcLow),log(ConcHigh),type="interval2") ~ 
+                                     DecYear+LogQ+SinDY+CosDY,data=Sam2,weights=weight,dist="gaus")
     
     return(survModel)
   }, error=function(e) {
